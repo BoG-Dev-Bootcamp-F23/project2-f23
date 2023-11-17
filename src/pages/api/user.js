4a2d64a565bdb9efa03cd3d22f89dcaf433909ab
Nathan Papa <nathanpapa35@gmail.com>
2023-11-16 19:05:52 -0500

    Add delete user

--- src/pages/api/user.js
+++ src/pages/api/user.js
@@ -1,3 +1,4 @@
+import deleteUser from "../../../server/mongodb/actions/deleteUser";
 import createUser from "../../../server/mongodb/actions/createUser";
 
 export default async function handler(req, res) {
@@ -6,6 +7,14 @@ export default async function handler(req, res) {
             await createUser(req.body);
         } catch {
             return res.status(500).send("Invalid data.");
+        }
+        return res.status(200).send("Success");
+    }
+    if (req.method === "DELETE") {
+        try {
+            await deleteUser(req.query);
+        } catch {
+            res.status(500).send("Invalid data.");
         }
         return res.status(200).send("Success");
     }

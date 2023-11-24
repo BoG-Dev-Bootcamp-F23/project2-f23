ae9568865bd0636accf27688220cc839a103c28d
Nathan Papa <nathanpapa35@gmail.com>
2023-11-23 22:39:57 -0600

    Login and Create Account page

---
+++ src/pages/Login.js
@@ -0,0 +1,27 @@
+import { useState, useEffect } from 'react';
+import styles from "../styles/Login.module.css";
+
+export default function Login() {
+
+    return (
+        <div className={styles.container}>
+            <div className={styles.navbar}>
+                <img src="pawprint.png" className={styles.pawprint} />
+                <h1>Progress</h1>
+            </div>
+            <div className={styles.loginBox}>
+                <h1>Login</h1>
+                <input type="email" placeholder='Email' className={styles.input} />
+                <input type="password" placeholder='Password' className={styles.input} />
+                <button type="button" className={styles.logInButton}>Log In</button>
+                <p>Don't have an account? <b className={styles.signUp}>Sign up</b></p>
+            </div>
+            <div className={styles.footer}>
+                <p>Made with ♡ by Team Elephant<br />© 2023 BOG Developer Bootcamp. All rights reserved.</p>
+            </div>
+            <svg xmlns="http://www.w3.org/2000/svg" className={styles.svg}>
+                    <circle className={styles.circle} cx="0" cy="100%" r="60%" />
+            </svg>
+        </div>
+    );
+}

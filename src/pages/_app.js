3db04c1fe73005d6b1b90201dabb3a5c9ffcd19b
Nathan Papa <nathanpapa35@gmail.com>
2023-11-23 22:43:12 -0600

    Fix potential merge conflicts

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,5 @@
 import '@/styles/globals.css'
-import CreateAccount from "./CreateAccount";
 
 export default function App({ Component, pageProps }) {
-  return <CreateAccount />
+  return <Component {...pageProps} />
 }

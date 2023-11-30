a3ef7e8234f14501486c5e34590b007aec3a348d
Lauren Ji <lyangji1011@gmail.com>
2023-11-29 23:13:16 -0500

    edit training log form styling

--- src/components/EditTrainingLog.js
+++ src/components/EditTrainingLog.js
@@ -1,10 +1,7 @@
 import React, { useState } from 'react';
 import styles from '../styles/CreateTrainingLog.module.css';
 import { useAuth } from "../contexts/useAuth"
-
-function isDateValid(dateStr) {
-    return !isNaN(new Date(dateStr));
-}
+import Head from 'next/head';
 
 export default function EditTrainingLog() {
     const {setDisplay, animals, editLog, setEditLog} = useAuth();
@@ -24,27 +21,11 @@ export default function EditTrainingLog() {
     const [day, setDay] = useState(old_date.getDate());
     const [year, setYear] = useState(old_date.getFullYear());
 
+    const [invalidHours, setInvalidHours] = useState(false);
+    const [invalidDate, setInvalidDate] = useState(false);
+    const [invalidYear, setInvalidYear] = useState(false);
+    const thirtyDays = ['April', 'June', 'September', 'November'];
     const [notes, setNotes] = useState(editLog.description);
-
-    async function handleSubmit() {
-        const param = {
-            trainingLogID: editLog._id,
-            user: editLog.user,
-            animal: editLog.animal,
-            title,
-            date: `${month} ${day}, ${year}`,
-            description: notes,
-            hours,
-        };
-        const response = await editlog(param);
-        console.log(response);
-        if (response.status === "success") {
-            setEditLog(null);
-            setDisplay(0);
-        } else {
-            //error handling
-        }
-    };
 
     async function handleDelete() {
         const param = {
@@ -84,69 +65,82 @@ export default function EditTrainingLog() {
         return data;
     }
 
+    function checkDateValidity(date) {
+        if (month === 'February') {
+            return date <= 29 && date >= 1
+        } else if (thirtyDays.includes(month)) {
+            return date <= 30 && date >= 1
+        } else {
+            return date <= 31 && date >= 1
+        }
+    }
+
     return (
         <div className={styles.trainingLogContainer}>
-        {/* <div> */}
-            <form onSubmit={handleSubmit} className={styles.formContainer}>
-            {/* <form onSubmit={handleSubmit} > */}
-                <label htmlFor="title">Title:</label>
-                <input type="text" id="title" placeholder="Title" value={title} onChange={(e) => setTitle(e.target.value)} required />
+            <Head>
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+                <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@300;500;700&display=swap" rel="stylesheet" />
+            </Head>
+            <form onSubmit={handleDelete} className={styles.formContainer}>
+                <label htmlFor="title" className={styles.titleText}>Title</label>
+                <input className={styles.input} type="text" id="title" placeholder="Title" value={title} onChange={(e) => setTitle(e.target.value)} required />
 
-                <label htmlFor="animal">Select Animal:</label>
-                {/*
-                <select id="animal" value={animal} onChange={(e) => setAnimal(e.target.value)} required>
-                    {animals.map(animal => (
-                        <option value={animal._id}>{animal.name} - {animal.breed}</option>
-                    ))}
-                </select>
-                */}
-                <select id="animal" value={animal} onChange={(e) => setAnimal(e.target.value)} required>
+                <label htmlFor="animal" className={styles.titleText}>Select Animal</label>
+                <select className={styles.input} id="animal" value={animal} onChange={(e) => setAnimal(e.target.value)} required>
                     {filteredAnimals.map(animal => (
                         <option value={animal._id}>{animal.name} - {animal.breed}</option>
                     ))}
                 </select>
 
-                <label htmlFor="hours">Total Hours Trained:</label>
-                <input type="number" id="hours" value={hours} onChange={(e) => setHours(e.target.value)} min="0" step="0.5" required />
+                <label htmlFor="hours" className={styles.titleText}>Total hours trained</label>
+                <input className={styles.input} type="number" id="hours" value={hours} onChange={(e) => {
+                    setHours(e.target.value)
+                    if (e.target.value < 0) {
+                        setInvalidHours(true);
+                    } else {
+                        setInvalidHours(false);
+                    }
+                }} min="0" step="0.5" required />
 
-                <label>Month</label>
-                <select id="month" value={month} onChange={(e) => setMonth(e.target.value)} required>
-                    <option value="January">January</option>
-                    <option value="February">February</option>
-                    <option value="March">March</option>
-                    <option value="April">April</option>
-                    <option value="May">May</option>
-                    <option value="June">June</option>
-                    <option value="July">July</option>
-                    <option value="August">August</option>
-                    <option value="September">September</option>np
-                    <option value="October">October</option>
-                    <option value="November">November</option>
-                    <option value="December">December</option>
-                </select>
+                <div className={styles.mdy}>
+                    <div className={styles.month}>
+                        <label className={styles.titleText}>Month</label>
+                        <select className={styles.input} id="month" value={month} onChange={(e) => setMonth(e.target.value)} required>
+                            <option value="January">January</option>
+                            <option value="February">February</option>
+                            <option value="March">March</option>
+                            <option value="April">April</option>
+                            <option value="May">May</option>
+                            <option value="June">June</option>
+                            <option value="July">July</option>
+                            <option value="August">August</option>
+                            <option value="September">September</option>np
+                            <option value="October">October</option>
+                            <option value="November">November</option>
+                            <option value="December">December</option>
+                        </select>
+                    </div>
+                    <div className={styles.day}>
+                        <label className={styles.titleText}>Date</label>
+                        <input className={styles.input} type="number" id="day" value={day} onChange={(e) => setDay(e.target.value)} min="1" max="31" required />
+                    </div>
+                    <div className={styles.year}>
+                        <label className={styles.titleText}>Year</label>
+                        <input className={styles.input} type="number" id="year" value={year} onChange={(e) => setYear(e.target.value)} min="1900" max="2100" required />
+                    </div>
+                </div>
 
-                <label>Date</label>
-                <input type="number" id="day" value={day} onChange={(e) => setDay(e.target.value)} min="1" max="31" required />
+                <label className={styles.titleText} htmlFor="notes">Note</label>
+                <textarea className={styles.input + " " + styles.textarea} id="notes" value={notes} placeholder="Note" onChange={(e) => setNotes(e.target.value)} rows="4"></textarea>
 
-                <label>Year</label>
-                <input type="number" id="year" value={year} onChange={(e) => setYear(e.target.value)} min="1900" max="2100" required />
+                <div className={styles.twoButtons}>
+                    <button className={styles.cancel} onClick = {() => {
+                        setDisplay(0);
+                    }}>Cancel</button>
 
-                <label htmlFor="notes">Note:</label>
-                <textarea id="notes" value={notes} placeholder="Note" onChange={(e) => setNotes(e.target.value)} rows="4"></textarea>
-
-                <button onClick = {() => {
-                    setDisplay(0);
-                }}>Cancel</button>
-
-                <button onClick = {(e) => {
-                    e.preventDefault();
-                    handleDelete();
-                }}> Delete </button>
-
-                <button type="submit" onClick = {(e) => {
-                    e.preventDefault();
-                    handleSubmit();
-                }}>Submit</button>
+                    <button className={styles.submit} type="submit">Delete</button>
+                </div>
             </form>
         </div>
     );
